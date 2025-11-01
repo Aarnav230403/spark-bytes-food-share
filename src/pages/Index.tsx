cd9e2d61ca1e63713de6744843208a04c6430c5c
Gina Carpenter <gcarp@bu.edu>
2025-11-01 17:49:28 -0400

    changed document title

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,8 +5,14 @@ import HowItWorks from "@/components/HowItWorks";
 import ImpactStats from "@/components/ImpactStats";
 import Team from "@/components/Team";
 import Footer from "@/components/Footer";
+import { useEffect } from "react";
 
 const Index = () => {
+
+  useEffect(() => { //sets tab header on browser to terriertable
+    document.title = "TerrierTable";
+  }, []);
+
   return (
     <main className="min-h-screen">
       <Navbar />
