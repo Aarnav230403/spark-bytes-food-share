7aa7ff19bb7fd48b9d9e44a751288c6b0027892c
Gina Carpenter <gcarp@bu.edu>
2025-12-02 21:06:33 -0500

    typo fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -178,7 +178,7 @@ export default function Header() {
                   </AvatarFallback>
                 </Avatar>
               </div>
-            </Dropdown>>
+            </Dropdown>
           </div>
         </div>
       </header>
